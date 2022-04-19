2c5ca0d71ffbed77bfc1466d39a8ad1fb06342df
yashwankar <yashwankar25@gmail.com>
2022-04-19 20:56:10 +0530

    Prograd ID : 8779

--- src/app.js
+++ src/app.js
@@ -24,25 +24,47 @@ const PLAYERS = [
 // initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = [];
-    // Create players using for loop
-    // Type your code here
-
+    for(var i=0;i<players.length;i++)
+    {
+        detailedPlayers[i]={
+            name:players[i],
+            strength:getRandomStrength(),
+            image:"images/super-"+(i+1)+".png"
+        };
+        if(i % 2 != 0)
+            detailedPlayers[i].type="hero";
+        else
+            detailedPlayers[i].type="villain";
+    }
     return detailedPlayers;
 }
 
 // getting random strength
 const getRandomStrength = () => {
-    // Return a random integer (0,100]
-    // Note: You can use Math.random() and Math.ceil()
+
+    let s = Math.ceil(Math.random() * (100-1) + 1);
+    return s;
 }
 
 const buildPlayers = (players, type) => {
     let fragment = '';
 
-    // Loop through players and accumulate HTML template
-    // depending of type of player(hero|villain)
-    // Type your code here
-
+    for(var i=0;i<players.length;i++)
+    {
+        if(players[i].type == type)
+        {
+            fragment+=`
+            <div class="player">
+                <img src="${players[i].image}" alt="">
+                <div class="name">
+                    ${players[i].name}
+                </div> 
+                <div class="strength">
+                    ${players[i].strength}
+                </div> 
+            </div>`;
+        } 
+    }
     return fragment;
 }
 // Display players in HTML
